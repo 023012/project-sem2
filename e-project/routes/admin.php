38cd592c81e2a5f51a9d9ba1d3ac1f293c13dd15
023012 <ntd.dat1230@gmail.com>
2023-08-20 10:13:42 +0700

    product comments

--- e-project/routes/admin.php
+++ e-project/routes/admin.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\HomeController;
-
+//use App\Http\Controllers\Admin\HomeController;
 //use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\LoginController;
 use App\Http\Controllers\Admin\UserController;
