3f2083acda80c82f1e645607240c958e7a371ea1
023012 <ntd.dat1230@gmail.com>
2023-08-26 12:01:00 +0700

    liên hệ

--- e-project/resources/views/site/pages/contact-us.blade.php
+++ e-project/resources/views/site/pages/contact-us.blade.php
@@ -1,159 +1,158 @@
 @extends('site.app')
 @section('content')
-<!-- ...:::: Start Breadcrumb Section:::... -->
-<div class="breadcrumb-section breadcrumb-bg-color--golden">
-    <div class="breadcrumb-wrapper">
+    <!-- ...:::: Start Breadcrumb Section:::... -->
+    <div class="breadcrumb-section breadcrumb-bg-color--golden">
+        <div class="breadcrumb-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h3 class="breadcrumb-title">Contact Us</h3>
+                        <div class="breadcrumb-nav breadcrumb-nav-color--black breadcrumb-nav-hover-color--golden">
+                            <nav aria-label="breadcrumb">
+                                <ul>
+                                    <li><a href="{{route('site.home')}}">Home</a></li>
+                                    <li class="active" aria-current="page">Contact Us</li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- ...:::: End Breadcrumb Section:::... -->
+
+    <!-- ...::::Start Map Section:::... -->
+    <div class="map-section" data-aos="fade-up" data-aos-delay="0">
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h3 class="breadcrumb-title">Contact Us</h3>
-                    <div class="breadcrumb-nav breadcrumb-nav-color--black breadcrumb-nav-hover-color--golden">
-                        <nav aria-label="breadcrumb">
+                    <div class="mapouter">
+                        <div class="gmap_canvas">
+                            <iframe id="gmap_canvas"
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.096609417893!2d105.77972177463558!3d21.028820080620488!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab86cece9ac1%3A0xa9bc04e04602dd85!2zRlBUIEFwdGVjaCBIw6AgTuG7mWkgLSBI4buHIFRo4buRbmcgxJDDoG8gVOG6oW8gTOG6rXAgVHLDrG5oIFZpw6puIFF14buRYyBU4bq_IChTaW5jZSAxOTk5KQ!5e0!3m2!1svi!2s!4v1692621892722!5m2!1svi!2s"
+                                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- ...::::End  Map Section:::... -->
+
+    <!-- ...::::Start Contact Section:::... -->
+    <div class="contact-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <!-- Start Contact Details -->
+                    <div class="contact-details-wrapper section-top-gap-100" data-aos="fade-up" data-aos-delay="0">
+                        <div class="contact-details">
+                            <!-- Start Contact Details Single Item -->
+                            <div class="contact-details-single-item">
+                                <div class="contact-details-icon">
+                                    <i class="fa fa-phone"></i>
+                                </div>
+                                <div class="contact-details-content contact-phone">
+                                    <a href="tel:+0123456789">Đường dây nóng</a>
+                                    <a href="tel:+0123456789">0123456789</a>
+                                    <a href="tel:+0123456789">0123456789</a>
+                                </div>
+                            </div> <!-- End Contact Details Single Item -->
+                            <!-- Start Contact Details Single Item -->
+                            <div class="contact-details-single-item">
+                                <div class="contact-details-icon">
+                                    <i class="fa fa-globe"></i>
+                                </div>
+                                <div class="contact-details-content contact-phone">
+                                    <a href="mailto:demo@example.com">Địa chỉ Email </a>
+                                    <a href="mailto:demo@example.com">demo@example.com</a>
+                                    <a href="http://www.example.com/">www.example.com</a>
+                                </div>
+                            </div> <!-- End Contact Details Single Item -->
+                            <!-- Start Contact Details Single Item -->
+                            <div class="contact-details-single-item">
+                                <div class="contact-details-icon">
+                                    <i class="fa fa-map-marker"></i>
+                                </div>
+                                <div class="contact-details-content contact-phone">
+                                    <span>Địa chỉ: 8 tôn thất thuyết Hà Nội </span>
+                                </div>
+                            </div> <!-- End Contact Details Single Item -->
+                        </div>
+                        <!-- Start Contact Social Link -->
+                        <div class="contact-social">
+                            <h4>Follow Us</h4>
                             <ul>
-                                <li><a href="{{route('site.home')}}">Home</a></li>
-                                <li class="active" aria-current="page">Contact Us</li>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
                             </ul>
-                        </nav>
-                    </div>
+                        </div> <!-- End Contact Social Link -->
+                    </div> <!-- End Contact Details -->
                 </div>
-            </div>
-        </div>
-    </div>
-</div> <!-- ...:::: End Breadcrumb Section:::... -->
-
-<!-- ...::::Start Map Section:::... -->
-<div class="map-section" data-aos="fade-up" data-aos-delay="0">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="mapouter">
-                    <div class="gmap_canvas">
-                        <iframe id="gmap_canvas" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.096609417893!2d105.77972177463558!3d21.028820080620488!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab86cece9ac1%3A0xa9bc04e04602dd85!2zRlBUIEFwdGVjaCBIw6AgTuG7mWkgLSBI4buHIFRo4buRbmcgxJDDoG8gVOG6oW8gTOG6rXAgVHLDrG5oIFZpw6puIFF14buRYyBU4bq_IChTaW5jZSAxOTk5KQ!5e0!3m2!1svi!2s!4v1692621892722!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> <!-- ...::::End  Map Section:::... -->
-
-<!-- ...::::Start Contact Section:::... -->
-<div class="contact-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4">
-                <!-- Start Contact Details -->
-                <div class="contact-details-wrapper section-top-gap-100" data-aos="fade-up" data-aos-delay="0">
-                    <div class="contact-details">
-                        <!-- Start Contact Details Single Item -->
-                        <div class="contact-details-single-item">
-                            <div class="contact-details-icon">
-                                <i class="fa fa-phone"></i>
-                            </div>
-                            <div class="contact-details-content contact-phone">
-                                <a href="tel:+0123456789">Đường dây nóng</a>
-                                <a href="tel:+0123456789">0123456789</a>
-                                <a href="tel:+0123456789">0123456789</a>
-                            </div>
-                        </div> <!-- End Contact Details Single Item -->
-                        <!-- Start Contact Details Single Item -->
-                        <div class="contact-details-single-item">
-                            <div class="contact-details-icon">
-                                <i class="fa fa-globe"></i>
-                            </div>
-                            <div class="contact-details-content contact-phone">
-                                <a href="mailto:demo@example.com">Địa chỉ Email </a>
-                                <a href="mailto:demo@example.com">demo@example.com</a>
-                                <a href="http://www.example.com/">www.example.com</a>
-                            </div>
-                        </div> <!-- End Contact Details Single Item -->
-                        <!-- Start Contact Details Single Item -->
-                        <div class="contact-details-single-item">
-                            <div class="contact-details-icon">
-                                <i class="fa fa-map-marker"></i>
-                            </div>
-                            <div class="contact-details-content contact-phone">
-                                <span>Địa chỉ: 8 tôn thất thuyết Hà Nội </span>
-                            </div>
-                        </div> <!-- End Contact Details Single Item -->
-                    </div>
-                    <!-- Start Contact Social Link -->
-                    <div class="contact-social">
-                        <h4>Follow Us</h4>
-                        <ul>
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                        </ul>
-                    </div> <!-- End Contact Social Link -->
-                </div> <!-- End Contact Details -->
-            </div>
-            <div class="col-lg-8">
-                
-            {{--              Notification                  --}}
-                                    @include('admin.partials.message')
-
-                <div class="contact-form section-top-gap-100" data-aos="fade-up" data-aos-delay="200">
-                    <h3>Get In Touch</h3>
-                    <form id="contact-form" action="{{ route('admin.contact.store') }}" method="post">
-                        @csrf
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="default-form-box mb-20">
-                                    <label for="contact-name">Họ tên</label>
-                                    <input name="name" type="text" class="form-control" placeholder="Khách hàng">
-                                    <span>
+                <div class="col-lg-8">
+                    <div class="contact-form section-top-gap-100" data-aos="fade-up" data-aos-delay="200">
+                        <h3>Get In Touch</h3>
+                        <form id="contact-form" action="{{ route('site.contactUs.sendContact') }}" method="post">
+                            @csrf
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="default-form-box mb-20">
+                                        <label for="contact-name">Họ tên</label>
+                                        <input name="name" type="text" class="form-control" placeholder="Khách hàng">
+                                        <span>
                                         @error('name')
                                         <div class="alert alert-danger">{{ $message }}</div>
                                         @enderror
                                     </span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="default-form-box mb-20">
-                                    <label for="contact-email">Số điện thoại</label>
-                                    <input name="phone" type="tel" class="form-control" placeholder="+(84) 978xxx">
-                                    <span>
+                                <div class="col-lg-6">
+                                    <div class="default-form-box mb-20">
+                                        <label for="contact-email">Số điện thoại</label>
+                                        <input name="phone" type="tel" class="form-control" placeholder="+(84) 978xxx">
+                                        <span>
                                         @error('phone')
                                         <div class="alert alert-danger">{{ $message }}</div>
                                         @enderror
                                     </span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-lg-12">
-                                <div class="default-form-box mb-20">
-                                    <label for="contact-subject">Email</label>
-                                    <input name="email" type="email" class="form-control" placeholder="Email">
-                                    <span>
+                                <div class="col-lg-12">
+                                    <div class="default-form-box mb-20">
+                                        <label for="contact-subject">Email</label>
+                                        <input name="email" type="email" class="form-control" placeholder="Email">
+                                        <span>
                                         @error('email')
                                         <div class="alert alert-danger">{{ $message }}</div>
                                         @enderror
                                     </span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-lg-12">
-                                <div class="default-form-box mb-20">
-                                    <label for="contact-message">Tin nhắn của bạn</label>
-                                    <textarea name="message" type="text" class="form-control" placeholder="Nội dung liên hệ"></textarea>
-                                    <span>
+                                <div class="col-lg-12">
+                                    <div class="default-form-box mb-20">
+                                        <label for="contact-message">Tin nhắn của bạn</label>
+                                        <textarea name="message" type="text" class="form-control"
+                                                  placeholder="Nội dung liên hệ"></textarea>
+                                        <span>
                                         @error('message')
                                         <div class="alert alert-danger">{{ $message }}</div>
                                         @enderror
                                     </span>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <button class="btn btn-lg btn-black-default-hover" type="submit">Gửi</button>
                                 </div>
                             </div>
-                            <div class="col-lg-12">
-                                <button class="btn btn-lg btn-black-default-hover" type="submit">Gửi</button>
-                                <a href="{{ route('site.pages.contact-us') }}"></a>
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<!-- ...::::ENd Contact Section:::... -->
+    <!-- ...::::ENd Contact Section:::... -->
 
 @endsection
--- e-project/routes/web.php
+++ e-project/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\User\HomeController;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,7 +29,6 @@ Route::group(['prefix' => ''], function () {
     Route::get('/blog-grid', [\App\Http\Controllers\User\BlogController::class, 'index'])->name('site.blog-grid-sidebar-left');
     Route::get('/blog-single', [\App\Http\Controllers\User\BlogController::class, 'blog_single_sidebar_left'])->name('site.blog-single-sidebar-left');
     Route::get('/about', [\App\Http\Controllers\User\AboutController::class, 'index'])->name('site.about-us');
-    Route::get('/contact', [\App\Http\Controllers\User\ContactController::class, 'index'])->name('site.contact-us');
     Route::get('/wishlist', [\App\Http\Controllers\User\WishlistController::class, 'index'])->name('site.wishlist');
     Route::get('/cart', [\App\Http\Controllers\User\CartController::class, 'index'])->name('site.cart');
     Route::get('/checkout', [\App\Http\Controllers\User\CheckoutController::class, 'index'])->name('site.checkout');
@@ -36,10 +36,12 @@ Route::group(['prefix' => ''], function () {
     Route::get('/compare', [\App\Http\Controllers\User\CompareController::class, 'index'])->name('site.compare');
     Route::get('/my_account', [\App\Http\Controllers\User\MyAccountController::class, 'index'])->name('site.my_account');
     Route::get('/login', [\App\Http\Controllers\User\LoginController::class, 'index'])->name('auth.login');
-});
-Route::group(['prefix' => 'contact-us'], function () {
 
-    Route::get('/', [\App\Http\Controllers\User\ContactController::class, 'index'])->name('site.pages.contact-us');
+
+    Route::get('/contact', [ContactController::class, 'index'])->name('site.contact-us');
+    Route::post('/contact', [ContactController::class, 'sendContact'])->name('site.contactUs.sendContact');
+
+
 });
 
 
