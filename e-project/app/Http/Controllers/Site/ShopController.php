1f2fc659645f0bd5e9c08b2cd31dada2d78dea18
023012 <ntd.dat1230@gmail.com>
2023-09-15 20:20:17 +0700

    fix

--- e-project/app/Http/Controllers/Site/ShopController.php
+++ e-project/app/Http/Controllers/Site/ShopController.php
@@ -26,67 +26,43 @@ class ShopController extends Controller
             'price-desc' => 'Giá từ cao xuống',
             'name-asc' => 'Tên tăng dần A-Z',
             'name-desc' => 'Tên giảm dần Z-A',
-//            'created-at-asc' => 'Ngày gần nhất',
-//            'created-at-desc' => 'Ngày xa nhất',
-//            'featured' => 'Đặc tính',
-
+            'created-at-asc' => 'Ngày gần nhất',
         ];
-//        dd($sortOptions);
         $sortOption = $request->input('sort_option');
 
-        // Kiểm tra giá trị tùy chọn và thực hiện xử lý tương ứng
         switch ($sortOption) {
             case 'price-asc':
                 $products = DB::table('project_sem2.products_list as p')
                     ->orderBy('p.price', 'asc')
-                    ->simplePaginate(15);
+                    ->simplePaginate(18);
                 break;
             case 'price-desc':
                 $products = DB::table('project_sem2.products_list as p')
                     ->orderBy('p.price', 'desc')
-                    ->simplePaginate(15);
+                    ->simplePaginate(18);
 
                 break;
             // Thêm các tùy chọn sắp xếp khác ở đây
             case 'name-asc':
                 $products=DB::table('project_sem2.products_list as p')
                     ->orderBy('p.name', 'asc')
-                    ->simplePaginate(15);
+                    ->simplePaginate(18);
                 break;
             case 'name-desc':
                 $products=DB::table('project_sem2.products_list as p')
                     ->orderBy('p.name', 'desc')
-                    ->simplePaginate(15);
+                    ->simplePaginate(18);
+                break;
+            case 'created-at-asc':
+                $products=DB::table('project_sem2.products_list as p')
+                    ->orderBy('p.created_at', 'asc')
+                    ->simplePaginate(18);
                 break;
 
-//            case 'created-at-asc':
-//                $products=DB::table('project_sem2.products_list as p')
-//                    ->orderBy('p.created_at', 'asc')
-//                    ->simplePaginate(15);
-//                break;
-//            case 'created-at-desc':
-//                $products=DB::table('project_sem2.products_list as p')
-//                    ->orderBy('p.created_at', 'desc')
-//                    ->simplePaginate(15);
-//                break;
-
-//            case 'featured':
-//                $products=DB::table('project_sem2.products_list as p')
-//                    ->orderBy('p.id', 'asc')
-//                    ->simplePaginate(16);
-//                break;
-
             default:
-                // Mặc định là sắp xếp theo giá từ thấp đến cao
-//                $products = DB::table('products')
-//                    ->select( 'categories.id as category_id', 'discounts.id as discount_id',
-//                        'categories.name as category', 'discounts.name as discount',
-//                        'discounts.discount_percent as discount_percent', 'products.*')
-//                    ->join('categories', 'category_id', '=', 'categories.id')
-//                    ->join('discounts', 'discount_id', '=', 'discounts.id')
-//                    ->orderBy('id','desc')
-                $products=DB::table('project_sem2.products_list')
-                    ->simplePaginate(15);
+                $products=DB::table('project_sem2.products_list as p')
+                    ->orderBy('p.created_at', 'asc')
+                    ->simplePaginate(18);
                 break;
         }
 
